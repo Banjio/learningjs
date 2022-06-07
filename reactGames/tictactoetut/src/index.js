aa48ae5985f6ae654f3faede3a5acd174c87675a
Maximilian Beier <beiermaximilian13@gmail.com>
2022-06-08 00:01:19 +0200

    Calculate winner function

--- reactGames/tictactoetut/src/index.js
+++ reactGames/tictactoetut/src/index.js
@@ -2,29 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+/* Writting suqare as class and not function component
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={()=> this.setState({value: 'X'})}>
-        {this.state.value}
+      <button 
+        className="square" 
+        onClick={()=> this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
+*/
+function Square(props){
+  return(
+    <button className="square" 
+    onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
+}
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i){
+    /* slice(start=0, end=len(arr)) gives a subset of an array*/
+    const squares = this.state.squares.slice();
+    // Shorthand if-else syntax
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares, 
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square 
+    value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+
+    />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '  + 
+    (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -48,6 +78,15 @@ class Board extends React.Component {
     );
   }
 }
+/*
+When a Square is clicked, the onClick function provided by the Board is called. Here’s a review of how this is achieved:
+
+The onClick prop on the built-in DOM <button> component tells React to set up a click event listener.
+When the button is clicked, React will call the onClick event handler that is defined in Square’s render() method.
+This event handler calls this.props.onClick(). The Square’s onClick prop was specified by the Board.
+Since the Board passed onClick={() => this.handleClick(i)} to Square, the Square calls the Board’s handleClick(i) when clicked.
+We have not defined the handleClick() method yet, so our code crashes. If you click a square now, you should see a red error screen saying something like “this.handleClick is not a function”.
+*/
 
 class Game extends React.Component {
   render() {
@@ -64,7 +103,26 @@ class Game extends React.Component {
     );
   }
 }
-
+// TODO: Can this be written more efficient?
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] == squares[b] & squares[a] == squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
